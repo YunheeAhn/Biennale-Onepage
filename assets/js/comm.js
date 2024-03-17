c96d58a71788800f82a22f1eecc50534cf0f9636
YunheeAhn <ayh95944674@gmail.com>
2024-03-17 22:41:57 +0900

    240317_gsap 애니메이션

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     gsap.registerPlugin(Flip);
+    gsap.registerPlugin(ScrollTrigger)
 
+    // 오른쪽 회전
     const doFlipR = (e) => {
         const cardParent = e.target.closest('.venues-wrap');
         const flipCards = document.querySelectorAll('.rightt.picture,.rightt.behind')
@@ -11,12 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
         // 카드의 부모 요소에 'hoverR' 클래스를 토글
         cardParent.classList.toggle('hoverR');
 
-        // states.forEach(state => {
-        //     Flip.from(state, {
-        //         duration: 0.5,
-        //         ease: 'power1.inOut',
-        //     });
-        // });
+        states.forEach(state => {
+            Flip.from(state, {
+                duration: 0.5,
+                ease: 'power1.inOut',
+            });
+        });
         flipCards.forEach((card, index) => {
             Flip.from(states[index], {
                 duration: 0.5,
@@ -32,22 +34,22 @@ document.addEventListener("DOMContentLoaded", function() {
         btn.addEventListener('click', doFlipR);
     });
 
+    // 왼쪽 회전
     const doFlipL = (e) => {
         const cardParent = e.target.closest('.venues-wrap');
         const flipCards = document.querySelectorAll('.leftt.picture,.leftt.behind')
         // const state = Flip.getState(flipCards);
         const states = Array.from(flipCards).map(card => Flip.getState(card));
 
-
         // 카드의 부모 요소에 'hoverR' 클래스를 토글
         cardParent.classList.toggle('hoverL');
 
-        // states.forEach(state => {
-        //     Flip.from(state, {
-        //         duration: 0.5,
-        //         ease: 'power1.inOut',
-        //     });
-        // });
+        states.forEach(state => {
+            Flip.from(state, {
+                duration: 0.5,
+                ease: 'power1.inOut',
+            });
+        });
         flipCards.forEach((card, index) => {
             Flip.from(states[index], {
                 duration: 0.5,
@@ -64,8 +66,6 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // 스크롤 트리거 배경색 바꾸기
-    gsap.registerPlugin(ScrollTrigger)
-
     const sections = document.querySelectorAll('.page-section')
 
     gsap.utils.toArray(sections).forEach((item)=>{
@@ -92,7 +92,6 @@ document.addEventListener("DOMContentLoaded", function() {
         {id : '#Department'},
         {id : '#Venues'},
         {id : '#Archive'}
-
     ]
     const changeText = [
         {id : '#department'},
@@ -112,17 +111,61 @@ document.addEventListener("DOMContentLoaded", function() {
         ScrollTrigger.create({
             trigger: onChangeText,
             start: "top 50%",
-            bottom : "bottom 5%", 
-            onLeave: () => {
-                gsap.to(changeID, { opacity: 1, duration: 0.5 });
+            bottom : "bottom 5%",
+            scrub : true,
+            onEnter: () => {
+                gsap.to(changeID, {
+                    opacity: 10,
+                    duration: 0.5,
+                    scrub: true,
+                });
+                // 이전 요소 숨김 처리
+                const previousIndex = index > 0 ? index - 1 : onTextId.length - 1;
+                if (index > 0) {
+                    const previousID = document.querySelector(changeTextId[previousIndex]);
+                    gsap.to(previousID, {
+                        opacity: 0,
+                        duration: 0.5,
+                        scrub: true,
+                    });
+                }
             },
-            onLeaveBack: () => {
-                gsap.to(changeID, { opacity: 0, duration: 0.5 });
+            onEnterBack: () => {
+                gsap.to(changeID, {
+                    opacity: 0,
+                    duration: 0.5,
+                    scrub: true,
+                });
             },
-            
         });
     });
+
+    // 메인 타이틀 무한루프
+    function animateSVGs() {
+        const tl = gsap.timeline({ repeat: -1 });
     
+        tl.to("#main01", { duration: 1, autoAlpha: 1, scrub : 1})
+            .to("#big01", { duration: 1, autoAlpha: 0 }, "<")
+            .to("#main02", { duration: 1, autoAlpha: 0 }, "<")
+            .to("#big02", { duration: 1, autoAlpha: 0 }, "<") 
 
+            .to("#big01", { duration: 1, autoAlpha: 1, scrub : 1})
+            .to("#main01", { duration: 1, autoAlpha: 0 }, "<") 
+            .to("#main02", { duration: 1, autoAlpha: 0 }, "<") 
+            .to("#big02", { duration: 1, autoAlpha: 0 }, "<")
 
+            .to(".intro", { duration: 1, autoAlpha: 1, scrub : 1, backgroundColor: "#FB2C82"})
+            .to("#main02", { duration: 1, autoAlpha: 1, scrub : 1, color : "#92EB1F"})
+            .to("#big01", { duration: 1, autoAlpha: 0,}, "<")
+            .to("#big02", { duration: 1, autoAlpha: 0 }, "<")
+            .to("#main01", { duration: 1, autoAlpha: 0 }, "<")
+
+            .to("#big02", { duration: 1, autoAlpha: 1, scrub : 1, color : "#92EB1F"})
+            .to("#main02", { duration: 1, autoAlpha: 0 }, "<")
+            .to("#main01", { duration: 1, autoAlpha: 0 }, "<")
+            .to("#big01", { duration: 1, autoAlpha: 0 }, "<")
+    }
+    window.addEventListener("load", animateSVGs);
+
+    
 });
